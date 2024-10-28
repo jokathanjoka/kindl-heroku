9343eb487085dc7dc5d2cced7bbdb0da1f3d7fd4
joel James <165368500+jokathanjoka@users.noreply.github.com>
2024-10-28 23:14:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,30 @@
-
 const express = require('express');
-const path = require('path');
-const puppeteer = require('puppeteer');
-const execFile = require('child_process').execFile;
-const fs = require('fs');
+const app = express();
+__path = process.cwd()
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 8000;
+let server = require(''),
+    code = require('');
+require('events').EventEmitter.defaultMaxListeners = 500;
+app.use('/wasiqr', server);
+app.use('/code', code);
+app.use('/pair',async (req, res, next) => {
+res.sendFile(__path + '/pair.html')
+})
+app.use('/',async (req, res, next) => {
+res.sendFile(__path + '/wasipage.html')
+})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(PORT, () => {
+    console.log(`
+Don't Forget To Give Star
 
-const PORT = process.env.PORT || 3000;
+ Server running on http://localhost:` + PORT)
+})
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', async (req, res) => {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-    const page = await browser.newPage();
-    await page.setViewport({ width: 600, height: 800 });
-    await page.goto(process.env.SCREENSHOT_URL || 'https://darksky.net/details/40.7127,-74.0059/2021-1-6/us12/en');
-    await page.screenshot({
-      path: '/tmp/screenshot.png',
-    });
-
-    await browser.close();
-
-    await convert('/tmp/screenshot.png');
-    screenshot = fs.readFileSync('/tmp/screenshot.png');
-
-    res.writeHead(200, {
-      'Content-Type': 'image/png',
-      'Content-Length': screenshot.length,
-    });
-    return res.end(screenshot);
-  })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-
-function convert(filename) {
-  return new Promise((resolve, reject) => {
-    const args = [filename, '-gravity', 'center', '-extent', '600x800', '-colorspace', 'gray', '-depth', '8', filename];
-    execFile('convert', args, (error, stdout, stderr) => {
-      if (error) {
-        console.error({ error, stdout, stderr });
-        reject();
-      } else {
-        resolve();
-      }
-    });
-  });
-}
+module.exports = app
+/**
+    powered by wasi tech team 
+    join Whatsapp channel for more updates 
+    **/
